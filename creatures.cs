b29b73e6f4658c00a86df996306d2d22a6d0e377
Oliver Elliott <146334137+OliE99@users.noreply.github.com>
2024-07-09 16:27:21 +0100

    Create creatures.cs

---
+++ creatures.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace ConsoleCreatures
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("   _   ,_,   _  ");
+            Console.WriteLine("  / `'=) (='` \\ ");
+            Console.WriteLine(" /.-.-.\\ /.-.-.\\ ");
+            Console.WriteLine("  `     '      `  ");
+        }
+    }
+}
